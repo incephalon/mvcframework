fc3f4c08ea654a46afabb8e523ee61642bd49aab
Cosmin Duta <noir2501@gmail.com>
2013-02-18 16:14:06 +0200

    Updated navigation database schema.

--- MVCFramework.Database/Tables/Navigation.sql
+++ MVCFramework.Database/Tables/Navigation.sql
@@ -1,8 +1,13 @@
 ﻿CREATE TABLE [dbo].[Navigation] (
-    [NavigationID] INT              NOT NULL,
-    [IsDefault]    BIT              CONSTRAINT [DF_Navigation_IsDefault] DEFAULT ((0)) NOT NULL,
+    [NavigationID] INT              IDENTITY (1, 1) NOT NULL,
     [Name]         VARCHAR (50)     NOT NULL,
-    [TenantUID]    UNIQUEIDENTIFIER NULL,
-    CONSTRAINT [PK_Navigation] PRIMARY KEY CLUSTERED ([NavigationID] ASC)
+    [TenantUID]    UNIQUEIDENTIFIER NOT NULL,
+    [RoleUID]      UNIQUEIDENTIFIER NULL,
+    CONSTRAINT [PK_Navigation] PRIMARY KEY CLUSTERED ([NavigationID] ASC),
+    CONSTRAINT [FK_Navigation_Role] FOREIGN KEY ([RoleUID]) REFERENCES [dbo].[Role] ([RoleUID]),
+    CONSTRAINT [FK_Navigation_Tenant] FOREIGN KEY ([TenantUID]) REFERENCES [dbo].[Tenant] ([TenantUID]),
+    CONSTRAINT [UQ_Navigation] UNIQUE NONCLUSTERED ([TenantUID] ASC, [RoleUID] ASC)
 );
+
+
 
--- MVCFramework.Database/Tables/NavigationItem.sql
+++ MVCFramework.Database/Tables/NavigationItem.sql
@@ -1,12 +1,16 @@
 ﻿CREATE TABLE [dbo].[NavigationItem] (
-    [NavigationItemID]       INT            NOT NULL,
-    [NavigationID]           INT            NOT NULL,
-    [ParentNavigationItemID] INT            NULL,
-    [ShowInMenu]             BIT            CONSTRAINT [DF_NavigationItem_ShowInMenu] DEFAULT ((1)) NOT NULL,
-    [Order]                  SMALLINT       CONSTRAINT [DF_NavigationItem_Order] DEFAULT ((0)) NOT NULL,
-    [Text]                   NVARCHAR (128) NOT NULL,
-    [Url]                    NVARCHAR (256) NULL,
-    [Icon]                   VARCHAR (256)  NULL,
-    CONSTRAINT [PK_NavigationItem] PRIMARY KEY CLUSTERED ([NavigationItemID] ASC)
+    [NavigationItemID] INT            IDENTITY (1, 1) NOT NULL,
+    [NavigationID]     INT            NOT NULL,
+    [ParentItemID]     INT            NULL,
+    [ShowInMenu]       BIT            CONSTRAINT [DF_NavigationItem_ShowInMenu] DEFAULT ((1)) NOT NULL,
+    [Order]            SMALLINT       CONSTRAINT [DF_NavigationItem_Order] DEFAULT ((0)) NOT NULL,
+    [Text]             NVARCHAR (128) NOT NULL,
+    [Url]              NVARCHAR (256) NULL,
+    [Icon]             VARCHAR (128)  NULL,
+    CONSTRAINT [PK_NavigationItem] PRIMARY KEY CLUSTERED ([NavigationItemID] ASC),
+    CONSTRAINT [FK_NavigationItem_Navigation] FOREIGN KEY ([NavigationID]) REFERENCES [dbo].[Navigation] ([NavigationID]),
+    CONSTRAINT [FK_NavigationItem_NavigationItem] FOREIGN KEY ([ParentItemID]) REFERENCES [dbo].[NavigationItem] ([NavigationItemID])
 );
+
+
 
